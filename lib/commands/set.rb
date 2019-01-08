69c0dbc0f7254a686beaf44915e8c05d92104934
Balazs Nadasdi <balazs.nadasdi@cheppers.com>
2019-01-08 10:29:19 +0100

    blind-try

--- lib/commands/set.rb
+++ lib/commands/set.rb
@@ -12,9 +12,27 @@ module MideaAirCondition
         @data[0x0b] = 0x42
       end
 
-      def temperature(celsius, mode: 2)
-        c = ((mode << 5) & 0xe0) | (celsius & 0xf) | ((celsius << 4) & 0x10)
+      def temperature(value, mode: 2)
+        c = ((mode << 5) & 0xe0) | (value & 0xf) | ((value << 4) & 0x10)
         @data[0x0c] = c
+      end
+
+      def unit(value)
+        unit_value = 0
+        unit_value = 1 if value == 'F'
+
+        # (byte)(
+        #     this.sleepFunc & 0x1
+        #   | this.tubro << 1 & 0x2
+        #   | this.tempUnit << 2 & 0x4
+        #   | this.catchCold << 3 & 0x8
+        #   | this.nightLight << 4 & 0x10
+        #   | this.peakElec << 5 & 0x20
+        #   | this.dusFull << 6 & 0x40
+        #   | this.cleanFanTime << 7 & 0x80)
+        # );
+        mask = 0xff ^ 0x4
+        @data[0x13] = (@data[0x13] & mask) | (unit_value << 2 & 0x4)
       end
 
       def fan_speed(speed)
